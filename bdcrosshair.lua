d3a3b89d699a45ff99be0f32d531fffb48fab207
R4zon59 <bukanw59@gmail.com>
2025-12-11 18:19:15 +0800

    Create bdcrosshair.lua

---
+++ bdcrosshair.lua
@@ -0,0 +1,15 @@
+local crs = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+
+crs.Name = "crs"
+crs.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+crs.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageLabel.Parent = crs
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.469596624, 0, 0.441588789, 0)
+ImageLabel.Size = UDim2.new(0.0602046959, 0, 0.116822429, 0)
+ImageLabel.Image = "rbxassetid://2392624578"
