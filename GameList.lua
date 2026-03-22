80a0f849a8db820a665a5519425bb110886e88ae
Wy5 <bukanw59@gmail.com>
2026-03-22 16:07:58 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,8 +1,8 @@
 local Games = {
-  [78041891124723] = "https://github.com/R4zon59/myrbxscript/blob/main/BloodDebtv2.lua", -- Blood Debt
-  [71895508397153] = "https://github.com/R4zon59/myrbxscript/blob/main/SigmasDODV2.lua", -- Die Of Death
-  [102871156420149] = "https://github.com/R4zon59/myrbxscript/blob/main/thelostfront.lua", -- The Lost Front
-  [123974602339071] = "https://github.com/R4zon59/myrbxscript/blob/main/test.lua" -- Baseplate
+  [78041891124723] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/BloodDebtv2.lua",
+  [71895508397153] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
+  [102871156420149] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
+  [123974602339071] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/test.lua"
 }
 
 return Games
