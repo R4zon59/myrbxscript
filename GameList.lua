0134df2e1edb9a98fcf5970049855143316a4bd9
Wy5 <bukanw59@gmail.com>
2026-03-22 16:44:44 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -2,7 +2,6 @@ local Games = {
   [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/BloodDebtv2.lua",
   [7344582593] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
   [102871156420149] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
-  [123974602339071] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/test.lua"
 }
 
 return Games
