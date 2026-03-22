9b2a365d20648ebc83d5eae1483d4865b17c5857
Wy5 <bukanw59@gmail.com>
2026-03-22 15:48:14 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,8 @@
 local Games = {
   [78041891124723, 127391603520196, 87541610558346] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blood Debt
   [71895508397153] = "https://github.com/R4zon59/myrbxscript/blob/main/SigmasDODV2.lua", -- Die Of Death
-  [102871156420149] = "https://github.com/R4zon59/myrbxscript/blob/main/thelostfront.lua" -- The Lost Front
+  [102871156420149] = "https://github.com/R4zon59/myrbxscript/blob/main/thelostfront.lua", -- The Lost Front
+  [123974602339071] = "
 }
 
 return Games
