445e103ca0a5a08e9668d78c3ed67e708fe76b68
Wy5 <bukanw59@gmail.com>
2026-03-22 16:43:15 +0800

    Update GameList with new game URL

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
   [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/BloodDebtv2.lua",
-  [71895508397153] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
+  [7344582593] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
   [102871156420149] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
   [123974602339071] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/test.lua"
 }
