3e08be8e119624ca026e455603e71b46e9fb7604
Wy5 <bukanw59@gmail.com>
2026-03-22 16:54:28 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,8 @@
 local Games = {
-  [7489122072, 8080423745] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/BloodDebtUnivv2.lua",
+  [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/BloodDebtUnivv2.lua", -- Blood Debt
+  [8080423745] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/BloodDebtUnivv2.lua", -- Blood Debt Gun Revamp
   [7344582593] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
-  [7935634976] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
+  [7935634976] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua"
 }
 
 return Games
