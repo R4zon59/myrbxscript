5a90492d4939f3d2a68707a47c57bb4404ca4452
Wy5 <bukanw59@gmail.com>
2026-03-22 16:22:09 +0800

    Update wy5main.lua

--- wy5main.lua
+++ wy5main.lua
@@ -1,7 +1,10 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/R4zon59/myrbxscript/main/GameList.lua"))()
 
 local URL = Games[game.GameId]
+print(game.GameID)
 
 if URL then
     loadstring(game:HttpGet(URL))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/wy5main.lua"))()
 end
