dbfa7d257c0689f4f77159118191a14f67f877bb
Wy5 <bukanw59@gmail.com>
2026-03-22 15:49:09 +0800

    Add new game link for Baseplate in GameList

--- GameList.lua
+++ GameList.lua
@@ -2,7 +2,7 @@ local Games = {
   [78041891124723, 127391603520196, 87541610558346] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blood Debt
   [71895508397153] = "https://github.com/R4zon59/myrbxscript/blob/main/SigmasDODV2.lua", -- Die Of Death
   [102871156420149] = "https://github.com/R4zon59/myrbxscript/blob/main/thelostfront.lua", -- The Lost Front
-  [123974602339071] = "
+  [123974602339071] = "https://github.com/R4zon59/myrbxscript/blob/main/test.lua" -- Baseplate
 }
 
 return Games
