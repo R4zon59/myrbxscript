f928d07cf3da5069020a9f19163e4f555c182346
Wy5 <bukanw59@gmail.com>
2026-03-22 15:46:25 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
   [78041891124723, 127391603520196, 87541610558346] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blood Debt
-  [71895508397153] = "", -- Die Of Death
+  [71895508397153] = "https://github.com/R4zon59/myrbxscript/blob/main/SigmasDODV2.lua", -- Die Of Death
 }
 
 return Games
