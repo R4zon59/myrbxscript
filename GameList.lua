ff7108dda74cf93e410ca5952addd9ee2ec0841e
Wy5 <bukanw59@gmail.com>
2026-03-22 16:45:36 +0800

    Update GameList with new game ID for thelostfront

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,7 @@
 local Games = {
   [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/BloodDebtv2.lua",
   [7344582593] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
-  [102871156420149] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
+  [7935634976] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
 }
 
 return Games
