87d6fc4be7b09edbf3221d15f769c4e2e454a5a5
Wy5 <bukanw59@gmail.com>
2026-03-22 15:42:53 +0800

    Add game URLs to GameList

--- GameList.lua
+++ GameList.lua
@@ -1,1 +1,5 @@
+local Games = {
+  [78041891124723, 127391603520196, 87541610558346] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blood Debt
+}
 
+return Games
