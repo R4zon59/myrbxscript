f24949020692811933b68dd2d83770c5f6d12f84
Wy5 <bukanw59@gmail.com>
2026-03-27 22:46:43 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,6 @@
 local Games = {
-  [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/BloodDebtUnivv2.lua", -- Blood Debt
-  [7344582593] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/SigmasDODV2.lua",
-  [7935634976] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/main/thelostfront.lua",
+  [7489122072] = "https://raw.githubusercontent.com/R4zon59/myrbxscript/refs/heads/main/BloodDebtUnivv2.lua", -- Blood Debt [Discountinued]
+  []
 }
 
 return Games
